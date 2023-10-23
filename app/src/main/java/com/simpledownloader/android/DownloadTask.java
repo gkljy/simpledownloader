65f8cc3d6e2b6f02f91b59cfac9eeb9c0df6df3e
Tiger <Tiger@ckn.com>
2023-10-23 15:31:46 +0800

    Fifth commit

--- app/src/main/java/com/simpledownloader/android/DownloadTask.java
+++ app/src/main/java/com/simpledownloader/android/DownloadTask.java
@@ -1,8 +1,23 @@
 package com.simpledownloader.android;
 
 import android.os.AsyncTask;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+
+    public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
 
     private DownloadListener listener;
     private boolean isPaused;
@@ -14,7 +29,84 @@ public class DownloadTask extends AsyncTask<String, Integer, Integer> {
 
     @Override
     protected Integer doInBackground(String... strings) {
-        return null;
+        long downloadedLength = 0;
+        InputStream inputStream = null;
+        RandomAccessFile savedFile = null;
+        String downloadUrl = strings[0];
+        String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+        String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+        File file = new File(fileName + directory);
+        if (file.exists()) {
+            downloadedLength = file.length();
+        }
+        long contentLength = 0;
+        try {
+            contentLength = getContentLength(downloadUrl);
+            if (contentLength == 0) {
+                return TYPE_FAILED;
+            } else if (contentLength == downloadedLength) {
+                return TYPE_SUCCESS;
+            }
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .addHeader("RANGE", "bytes=" + downloadedLength + "-")
+                    .url(downloadUrl)
+                    .build();
+            Response response = client.newCall(request).execute();
+            if (response != null) {
+                inputStream = response.body().byteStream();
+                savedFile = new RandomAccessFile(file, "rw");
+                savedFile.seek(downloadedLength);
+                byte[] b = new byte[1024];
+                int total = 0;
+                int len;
+                while ((len = inputStream.read(b)) != -1) {
+                    if (isCanceled) {
+                        return TYPE_CANCELED;
+                    } else if (isPaused) {
+                        return TYPE_PAUSED;
+                    } else {
+                        total += len;
+                        savedFile.write(b, 0, len);
+                        int progress = (int) ((downloadedLength + total) / contentLength * 100);
+                        publishProgress(progress);
+                    }
+                }
+                response.body().close();
+                return TYPE_SUCCESS;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (savedFile != null) {
+                    savedFile.close();
+                }
+                if (isCanceled && file != null) {
+                    file.delete();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return TYPE_FAILED;
+    }
+
+    private long getContentLength(String downloadUrl) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(downloadUrl)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response != null && response.isSuccessful()) {
+            long contentLength = response.body().contentLength();
+            response.close();
+            return contentLength;
+        }
+        return 0;
     }
 
 
